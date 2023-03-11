fede0409b4f54d632ed5128e94b9fc1a4b8e85a1
rose-jpg <okhuomonajayi54@gmail.com>
2023-03-11 17:25:23 +0100

    commit

--- src/Pages/EditWhale.js
+++ src/Pages/EditWhale.js
@@ -8,8 +8,8 @@ function EditWhale() {
   return (
     <div>
       <div className='flex justify-center   w-[100%] h-[100vh] flex-col px-4'>
-        <h2 className='flex items-center font-bold text-white text-[30px] font-lexend '>{`Hi,${details.username}`}</h2>
-        <div className='flex flex-col items-center gap-3 '>
+        <h2 className='flex items-center font-bold text-white text-[30px] font-lexend'>{`Hi,${details.username}`}</h2>
+        <div className='flex flex-col  gap-3  text-white'>
           <div className='flex w-[350px] sm:w-[397px] justify-start pl-2 rounded-[8px] items-center h-[39px] border-[1px] mt-3 sm:mt-0 border-[#838699]'>
             {details.address}
           </div>
