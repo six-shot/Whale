0fb24ac309733b3fb2bd87add0f92b851c46d9d3
rose-jpg <okhuomonajayi54@gmail.com>
2023-03-11 18:10:58 +0100

    commit

--- src/Pages/EditWhale.js
+++ src/Pages/EditWhale.js
@@ -10,20 +10,20 @@ function EditWhale() {
       <div className='flex justify-center   w-[100%] h-[100vh] flex-col px-4'>
         <h2 className='flex items-center font-bold text-white text-[30px] mb-3 font-lexend'>{`Hi,${details.username}`}</h2>
         <div className='flex flex-col  gap-3  text-white'>
-        Address:
-          <div className='flex w-[100%]  justify-start pl-2 rounded-[8px] items-center h-[39px] border-[1px] -mt-3  border-[#838699]'>
+       <h5 className="text-sm"> Address:</h5>
+          <div className='flex w-[100%]  justify-start pl-2 rounded-[8px] items-center h-[39px] border-[1px] -mt-2  border-[#838699]'>
         
             {details.address}
           </div>
-          Balance:
+         <h5 className="text-sm"> Balance: </h5>
           <div className='flex w-[100%] justify-start pl-2 rounded-[8px] items-center h-[39px] border-[1px] -mt-3 sm:mt-0 border-[#838699]'>
             {details.quantity}
           </div>
-          Percentage:
+           <h5 className="text-sm">Percentage:</h5>
           <div className='flex w-[100%]  justify-start pl-2 rounded-[8px] items-center h-[39px] border-[1px] -mt-3  sm:mt-0 border-[#838699]'>
             {details.percentage}
           </div>
-          Value:
+          <h5 className="text-sm">  Value: </h5>
           <div className='flexw-[100%]  justify-start pl-2 rounded-[8px] items-center h-[39px] border-[1px] -mt-3 sm:mt-0 border-[#838699]'>
             {details.value}
           </div>
