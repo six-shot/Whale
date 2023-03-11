ea28f7a30e8260f91436f659b73425e991aac9a3
rose-jpg <okhuomonajayi54@gmail.com>
2023-03-11 15:48:42 +0100

    commit

--- src/Pages/EditWhale.js
+++ src/Pages/EditWhale.js
@@ -9,7 +9,7 @@ function EditWhale() {
   return (
     <div >
       <div className="flex justify-center items-center h-[100vh]">
-      <div className="flex flex-col gap-3">
+      <div className="flex flex-col gap-3 text-white">
             <div className='flex w-[350px] sm:w-[397px] justify-start pl-2 rounded-[8px] items-center h-[39px] border-[1px] mt-3 sm:mt-0 border-[#838699]'>
              
              {details.address}
