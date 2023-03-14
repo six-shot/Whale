682b650f3ce849fddc164dd05c3f6603f0341914
rose-jpg <okhuomonajayi54@gmail.com>
2023-03-14 16:13:40 +0100

    commit

---
+++ src/data/App.js
@@ -0,0 +1,21 @@
+
+import "./App.css"
+import Home from "./Pages/Home"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import AddWhales from "./Pages/AddWhales"
+import EditWhale from "./Pages/EditWhale"
+function App() {
+  return (
+    <div className='w-full '>
+     <Router>
+      <Routes>
+        <Route path="/" exact  element={<Home/>}/>
+        <Route path="add-whales" element={<AddWhales/>}/>
+        <Route path='/details/:id' element={<EditWhale />} />
+      </Routes>
+     </Router>
+    </div>
+  )
+}
+
+export default App
